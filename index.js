e47c37099fd59a63c0b7d3eee22d74a441fdd905
nattapon <nattapon@eruditetech.net>
2018-07-17 16:35:26 +0700

    update

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const io = require('socket.io').listen(server)
 
 let socketList = {}
 let mapSocketIdToUsername = {}
+let mapSocketIdToRoomId = {}
 let roomList = {}
 let roomIndexNow = 0
 
@@ -35,39 +36,41 @@ io.on('connection', socket => {
   })
 
   socket.on('getAllRooms', () => {
-    console.log('getAllRooms')
     const rooms = Object.keys(roomList)
     socket.emit('roomUpdateAll', rooms)
   })
 
   socket.on('createRoom', () => {
-    console.log('createRoom')
     const roomId = createRoom(socket.id)
     io.emit('roomUpdate', roomId)
+    socket.emit('roomCreated', roomId)
   })
 
   socket.on('joinRoom', roomId => {
     socket.join(roomId)
+    mapSocketIdToRoomId[socket.id] = roomId
     socket.to(roomId).emit('someoneJoinRoom', mapSocketIdToUsername[socket.id])
     socket.emit('joinedRoom', roomId)
   })
 
-  socket.on('getBoardState', () => {
+  socket.on('leaveRoom', roomId => {
+    socket.leave(roomId)
+    delete mapSocketIdToRoomId[socket.id]
+    socket.to(roomId).emit('someoneLeaveRoom', mapSocketIdToRoomId[socket.id])
+    socket.emit('leavedRoom', roomId)
+  })
 
+  socket.on('getBoardState', () => {
+    socket.emit('updateWholeBoardState', roomList[mapSocketIdToRoomId[socket.id]].boardState)
   })
 
   socket.on('move', position => {
     socket.to(roomId).emit('moved', position)
   })
 
-  socket.on('leaveRoom', roomId => {
-    socket.leave(roomId)
-    socket.to(roomId).emit('someoneLeaveRoom', username)
-    socket.emit('leavedRoom', roomId)
-  })
-
   socket.on('disconnect', () => {
     delete socketList[socket.id]
+    delete mapSocketIdToUsername[socket.id]
     socket.removeAllListeners()
     console.log(socket.id, 'disconnected')
   })
